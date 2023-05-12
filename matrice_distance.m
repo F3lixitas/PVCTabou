cdbae79c115888f9487a5968421d90c10b775191
0MelissassileM0 <115464980+0MelissassileM0@users.noreply.github.com>
2023-05-12 09:02:11 +0200

    commentaires + correction sens matrice entrée

--- matrice_distance.m
+++ matrice_distance.m
@@ -1,13 +1,17 @@
 function [M]=matrice_distance(villes)
-    if size(villes,2) ~= 2
+% Entrée : ville : la matrice des positions des villes, qui a autant de
+%                  colonnes que de villes et 2 lignes pour x et y
+% Sortie : M : la matrice qui contient les distances entre chaque point de
+%              l'espace
+    if size(villes,1) ~= 2
         error("Erreur dans la fonction distance : Les villes doivent avoir des positions en 2 dimensions");
     end
-    s = size(villes,1);
-    temp = zeros(s+1,2);
-    temp(2:s+1,:)=villes;
+    s = size(villes,2);
+    temp = zeros(2,s+1);
+    temp(:,2:s+1)=villes;
     for i=1:s+1
         for j=1:s+1
-            M(i,j) = norm(temp(i,:) - temp(j,:));
+            M(i,j) = norm(temp(:,i) - temp(:,j));
         end
     end
 end
