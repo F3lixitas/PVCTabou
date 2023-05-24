2ac3986cea968cab5fd8138cb29cb7141fb5d418
Felix <felixriedel25@gmail.com>
2023-05-24 13:35:03 +0200

    tests

--- PVCTabou.m
+++ PVCTabou.m
@@ -1,4 +1,7 @@
-function [ordre_optimal]=PVCTabou(villes, nBIterations)
+function [ordre_optimal]=PVCTabou(villes, nBIterations, plageTabou)
+% villes 
+% nBIterations : nombre maximal d'itérations
+% plageTabou : plage de valeurs de verrouillage du tableau tabou (tableau à 2 valeurs)
     ordre=randperm(size(villes,2));
     ordre_optimal=ordre;
     distance_optimale=distance(ordre_optimal, villes);
@@ -6,7 +9,7 @@ function [ordre_optimal]=PVCTabou(villes, nBIterations)
     T=zeros(size(villes, 2), size(villes, 2));
     for i=1:nBIterations
         deltaP = calculDeltaP(ordre, D);
-        [T,ordre] = select(deltaP, T, i, ordre, floor(randn * 5 + 5));
+        [T,ordre] = select(deltaP, T, i, ordre, floor(randn * (plageTabou(2)-plageTabou(1)) + plageTabou(1)));
         dist = distance(ordre, villes);
         if(dist < distance_optimale)
             ordre_optimal = ordre;
--- main.m
+++ main.m
@@ -1,18 +1,53 @@
 rand('state', sum(100*clock));
 % Entrée de test
 
-villes = [ 1, 2, 1, 6, 7, 2, 4, 9, 3;
-           1, 1, 2, 0, 4, 5, 3, 2, 7];
+villes = [ 1, 2, 1, 6, 7, 2, 4, 9, 3, 8, 1, 4,6 , 3, 7, 4, -1, 3, 7, -6, 5, -5, 1, 2, 9, 6, -4, -2, 4, 7;
+           1, 1, 2, 0, 4, 5, 3, 2, 7, 7, 5, 1, 9, 4, 3, 5, 7, -2, 8, 3, -7, -3, -1, 4, 8, 1, 3, -8, 6, 1];
 
 %find(villes(1,:) == 2)
 
-ordre=PVCTabou(villes, 1000)
-villes_ordonnees = zeros(size(villes,1), size(villes, 2)+2);
-for k=1:size(ordre,2)
-    villes_ordonnees(:,k+1) = villes(:, ordre(k));
+%villes = rand(2, 30).*10;
+
+dist = [];
+t = [];
+it = 1000000;
+
+opti = [];
+distOpti = -1;
+
+for n = 1:100
+    tic
+    ordre=PVCTabou(villes, it, [30, 60]);
+    t(n)=toc;
+
+    dist(n) = distance(ordre, villes);
+    if distOpti < 0
+        distOpti = distance(ordre, villes);
+        opti = ordre;
+    elseif dist(n) < distOpti
+        distOpti = distance(ordre, villes);
+        opti = ordre;
+    end
+
+    
 end
 
-distance(ordre, villes)
+opti
+
+mean_d=mean(dist)
+std_d=std(dist)
+min_d=min(dist)
+max_d=max(dist)
+
+mean_t=mean(t*1000)
+std_t=std(t*1000)
+min_t=min(t*1000)
+max_t=max(t*1000)
+
+villes_ordonnees = zeros(size(villes,1), size(villes, 2)+2);
+for k=1:size(opti,2)
+    villes_ordonnees(:,k+1) = villes(:, opti(k));
+end
 
 figure;
 plot(villes(1,:), villes(2,:), '+');
